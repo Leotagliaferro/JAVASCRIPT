9a4e43a77ba944ff0c4b8feaf41937d8042ff309
Leotagliaferro <111162057+Leotagliaferro@users.noreply.github.com>
2023-03-29 12:57:31 -0400

    Atividade

---
+++ Aula 5/ex008.js
@@ -0,0 +1,7 @@
+var vel = 72.2
+console.log(`A velocidade do seu carro é de ${vel} km por hora`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usandoo um cinto de segurança')
---
+++ Aula 5/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`vivendo em ${pais} `)
+if (pais == 'brasil') {
+    console.log('Você é brasileiro Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
